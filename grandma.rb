60cc27d18362ac55850ca9cd789b2422a7882b1a
Lisa Walters <campi_l@fortlewis.edu>
2020-08-19 05:21:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,24 +1,26 @@
-def speak_to_grandma
-  print "Say something to Grandma."
+print "Say something to Grandma."
   speak = gets.chomp
 
   if
     speak == speak.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+    print "HUH?! SPEAK UP, SONNY!"
+
+    elsif
+
+    speak == "I LOVE YOU GRANDMA!"
+    print "I LOVE YOU TOO PUMPKIN!" 
 
   elsif
   speak == speak.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
+    print "HUH?! SPEAK UP, SONNY!"
 
   elsif
 
     speak == speak.upcase
-    return "NO, NOT SINCE 1938!"
+    print "NO, NOT SINCE 1938!"
 
-  else
-
-    speak == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-
-
+  
 end
+
+
+
