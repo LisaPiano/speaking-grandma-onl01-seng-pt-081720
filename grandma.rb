7f83ce00bad55a22e27a85181a5a2d29cb26591e
Lisa Walters <campi_l@fortlewis.edu>
2020-08-19 05:37:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,22 +1,3 @@
 def speak_to_grandma(speak)
 
-  if
-    speak == speak.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-
-    elsif
-
-    speak == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-
-  elsif
-  speak == speak.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
-
-  elsif
-
-    speak == speak.upcase
-    print "NO, NOT SINCE 1938!"
-
-
-end
+  
